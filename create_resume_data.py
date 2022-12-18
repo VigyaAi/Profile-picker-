b73e1faa76466b1695fe3dc5baf797ad4bbfc14d
[Avinash Dubey] <avinashdubeyg2001@gmail.com>
2022-12-18 17:27:51 +0530

    From pdf's data extraction and saving done

--- create_resume_data.py
+++ create_resume_data.py
@@ -1,32 +1,44 @@
+#This file reads pdfs in a folder, read their texts
+#creates a table which is saved in data/resume_data.csv
+
+
 # importing required modules
 import os
 import PyPDF2
 import pandas as pd
 
-data_path = r"resume_archive\pdf"
+data_path = r"data\pdf_data"
 
+#####################
+#Initializing the cols
+pdf_path, pdf_id, pdf_text = [], [], []
 
 for pdf_name in os.listdir(data_path):
+    print(f"Reading data from: {pdf_name}")
+    temp_path = os.path.join(data_path, pdf_name)
 
-    pdf_path = os.path.join(data_path, pdf_name)
-
-   
+    pdf_path.append(temp_path)
     # creating a pdf file object
-    pdfFileObj = open(pdf_path, 'rb')
+    pdfFileObj = open(temp_path, 'rb')
    
     # creating a pdf reader object
     pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
    
     # printing number of pages in pdf file
-    print(pdfReader.numPages)
+    # print(pdfReader.numPages)
    
     # creating a page object
     pageObj = pdfReader.getPage(0)
    
-    # extracting text from page
-    print(pageObj.extractText())
-   
+    # extracting text and idfrom page
+    pdf_text.append(pageObj.extractText())
+    pdf_id.append(pdf_name.split(".")[0])
+
+
     # closing the pdf file object
     pdfFileObj.close()
 
-    break
+#creating the dataframe and saving the data
+df = pd.DataFrame({"pdf_path":pdf_path, "id":pdf_id, "text":pdf_text})
+
+df.to_csv("data/resume_data.csv", index=False)
