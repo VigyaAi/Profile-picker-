94ef7e70cf0a4266d56ba4aff5e8c8bbbc073f91
Aman <amanasstudent@gmail.com>
2022-12-19 15:32:06 +0530

    UI Updated

--- ui/js/index.js
+++ ui/js/index.js
@@ -1,18 +1,3 @@
-function myFunction() {
-  // Get the text field
-  const copyText = document.getElementById("output-text");
-
-  // Select the text field
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); // For mobile devices
-
-  // Copy the text inside the text field
-  navigator.clipboard.writeText(copyText.value).then((r) => r);
-
-  // Alert the copied text
-  alert("Copied the text: " + copyText.value);
-}
-
 const sendRequest = () => {
   /** TODO
    *  1) run python code
@@ -25,33 +10,116 @@ const sendRequest = () => {
 
   const response = [
     {
-      score: 10,
-      link: "example.pdf",
+      serialNum: 1,
+      score: 90,
+      link: "example1.pdf",
     },
     {
-      score: 10,
-      link: "example.pdf",
+      serialNum: 2,
+      score: 80,
+      link: "example2.pdf",
     },
     {
-      score: 10,
-      link: "example.pdf",
+      serialNum: 3,
+      score: 70,
+      link: "example3.pdf",
     },
     {
-      score: 10,
-      link: "example.pdf",
+      serialNum: 4,
+      score: 60,
+      link: "example4.pdf",
     },
     {
-      score: 10,
-      link: "example.pdf",
+      serialNum: 5,
+      score: 50,
+      link: "example5.pdf",
     },
   ];
 
   response.map((item, index) => {
-    // <h1>${item.score}</h1><p>${item.link}</p>
-    const outputSelector = document.getElementsByClassName("output")[0];
+
+    const outputSelector = document.getElementById("output");
     outputSelector.insertAdjacentHTML(
-      "afterbegin",
-      `<h1 class="output-score">${item.score}</h1><p class="output-link">${item.link}</p>`
+      "beforeend",
+      `<th class="output-serialNum">${item.serialNum}</th><th class="output-link">${item.link}</th><th class="output-score">${item.score}</th>`
     );
+    console.log(item);
   });
 };
+
+
+
+
+// console.clear();
+
+// let angle = 3.14/4;
+// let length = 2;
+
+// let particles = [];
+
+// let mouseHasMoved = false;
+
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+//   colorMode(HSB);
+//   background(0, 0, 0);
+//   noStroke();
+//   angle = PI/3;
+//   for (let i=0; i<500; i++) {
+//     new Particle(createVector(random(width), random(height)));
+//   }
+// }
+
+// function draw() {
+//   if (!mouseHasMoved) {
+//     for (let i=0; i<5; i++) {
+//       new Particle(createVector(random(width), random(height)));
+//     }
+//   }
+//   background(0, 0, 0, .1);
+//   for (let p of particles) {
+//     p.update();
+//     p.draw();
+//   }
+// }
+
+// function mouseMoved() {
+//   mouseHasMoved = true;
+//   for (let i=0; i<5; i++) {
+//     new Particle(createVector(mouseX, mouseY));
+//   }
+// }
+
+// class Particle {
+//   constructor(pos) {
+//     this.pos = pos;
+//     this.vel = createVector(random(length*.5, length*1.5), 0).rotate(angle * round(random(0, 360)));
+//     this.hue = random(90, 150);
+//     this.sat = 100;
+//     this.val = 100;
+//     this.lifetime = random(50, 150);
+//     this.age = 0;
+//     particles.push(this);
+//   }
+  
+//   update() {
+//     if (this.age >= this.lifetime) {this.remove()}
+//     this.age += 1;
+//     if (this.age % 10 == 0) {
+//       this.vel.rotate(random([-angle, angle]));
+//     }
+//     this.pos.add(this.vel);
+//   }
+  
+//   draw() {
+//     push()
+//     stroke(this.hue, this.sat, this.val, 1-this.age/this.lifetime);
+//     translate(this.pos.x, this.pos.y);
+//     line(0, 0, -this.vel.x, -this.vel.y);
+//     pop()
+//   }
+  
+//   remove() {
+//     particles.splice(particles.indexOf(this), 1);
+//   }
+// }
