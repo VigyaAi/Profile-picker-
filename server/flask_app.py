b39c9eb187ddc7e9ecb9c1257a1554e2669dfedf
[Avinash Dubey] <avinashdubeyg2001@gmail.com>
2022-12-20 17:45:29 +0530

    flask corse error resolved

--- server/flask_app.py
+++ server/flask_app.py
@@ -1,14 +1,23 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 from resume_sorter import *
 
 
 app = Flask(__name__)
+CORS(app, support_credentials=True)
 
 @app.route('/query')
+@cross_origin(supports_credentials=True)
 def hello_world():
     #Description will be read by the below command ✅
     description = request.args.get('description')
-    return sort_resume(description)
+    sort_res = sort_resume(description)
+    print(jsonify(sort_res))
+
+    return jsonify(sort_res)
+    
+
+
 
 if __name__ == '__main__':
-   app.run()
+   app.run(port=5000)
