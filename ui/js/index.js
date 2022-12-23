cf5587676156780464fecbeade500290c1379a13
itssabhishek <itsabhishek0710@gmail.com>
2022-12-23 10:05:59 +0530

    Fixed responsiveness

--- ui/js/index.js
+++ ui/js/index.js
@@ -1,13 +1,12 @@
 function sendRequest() {
   event.preventDefault();
-  
+
   // 1) call api
 
   const submitButton = document.getElementsByClassName("submit-button")[0];
   submitButton.style.display = "none";
   const loaderButton = document.getElementsByClassName("loader")[0];
   loaderButton.style.display = "flex";
-
 
   const mainBody = document.getElementsByTagName("body")[0];
 
@@ -33,8 +32,8 @@ function sendRequest() {
   const outputSelector = document.getElementsByClassName("output")[0];
 
   response(description).then((data) => {
-     submitButton.style.display = "inline-block";
-     loaderButton.style.display = "none";
+    submitButton.style.display = "inline-block";
+    loaderButton.style.display = "none";
 
     data.map((item, index) => {
       outputSelector.insertAdjacentHTML(
@@ -55,37 +54,37 @@ function sendRequest() {
 }
 
 function auto_grow(element) {
-  // const submitButton = document.getElementsByClassName("submit-button")[0];
+  const submitButton = document.getElementsByClassName("submit-button")[0];
 
-  // const query = document.getElementsByClassName("query_text")[0];
-  // if ((query.value && event && event.keyCode === 13) || event === 0) {
-  //   submitButton.click();
-  // }
+  const query = document.getElementsByClassName("query_text")[0];
+  if ((query.value && event && event.keyCode === 13) || event === 0) {
+    submitButton.click();
+  }
 
-  element.style.height = 'auto';
+  element.style.height = "auto";
   element.style.height = element.scrollHeight + "px";
 }
 
 // Voice to Text :
 
-click_to_record.addEventListener('click',function(){
+click_to_record.addEventListener("click", function () {
   var speech = true;
   window.SpeechRecognition = window.webkitSpeechRecognition;
 
   const recognition = new SpeechRecognition();
   recognition.interimResults = true;
 
-  recognition.addEventListener('result', e => {
-      const transcript = Array.from(e.results)
-          .map(result => result[0])
-          .map(result => result.transcript)
-          .join('')
+  recognition.addEventListener("result", (e) => {
+    const transcript = Array.from(e.results)
+      .map((result) => result[0])
+      .map((result) => result.transcript)
+      .join("");
 
-      document.getElementById("convert_text").innerHTML = transcript;
-      console.log(transcript);
+    document.getElementById("convert_text").innerHTML = transcript;
+    console.log(transcript);
   });
-  
+
   if (speech == true) {
-      recognition.start();
+    recognition.start();
   }
-})
+});
